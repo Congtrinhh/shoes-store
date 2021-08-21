bfbfe4b10acf7a6a875a1ba063d5320bf2d6d0d1
Congtrinhh <trinhquycong@gmail.com>
2021-08-21 21:08:20 +0700

    evening, everything else let's do tomorrow

---
+++ login/js/main.js
@@ -0,0 +1,26 @@
+$('#userForm').validate({
+    messages: {
+        name: "Vui lòng nhập tên tài khoản",
+        password: "Vui lòng nhập mật khẩu",
+    },
+    errorPlacement: function(errorElement, invalidElement) {
+        errorElement.insertAfter( invalidElement.parent() );
+    }
+});
+
+$('.btn').on('click', function(e){
+    e.preventDefault();
+    if ( $('#userForm').valid() ) {
+        if ( $('[name="name"]').val() =='tomcat' && $('[name="password"]').val() =="12345" ) {
+            console.log("hop le");
+            window.location.assign('./index.html');
+
+            localStorage.setItem('userName', 'tomcat');
+            localStorage.setItem('login-token', 'user');
+        }
+        else {
+            console.log("k hop le");
+            location.reload();
+        }
+    }
+})
