ff6ce115875f0a1480e39a96a7bcb10e6e3c7ec8
Congtrinhh <trinhquycong@gmail.com>
2021-09-06 10:32:39 +0700

    changes

--- user_info/js/main.js
+++ user_info/js/main.js
@@ -9,52 +9,42 @@
 // })
 
 
-$('form').on('click', function(){
-	$('.error-message').removeClass('on');
+$('form').on('click', function () {
+    $('.error-message').removeClass('on');
 })
 
 // ---------------- ẩn / hiện nút đổi mk ----------------
-$('.btn-show').on('click', function(e) {
+$('.btn-show').on('click', function (e) {
     $('.update-password-area').toggleClass('on');
     $(this).toggleClass('on');
-    if ( $(this).hasClass('on') ) {
+    if ($(this).hasClass('on')) {
         $(this).text('Hủy')
-		$('[name="current-password"]').val(null);
-		$('[name="new-password"]').val(null);
-		$('[name="confirmed-password"]').val(null);
+        $('[name="current-password"]').val(null);
+        $('[name="new-password"]').val(null);
+        $('[name="confirmed-password"]').val(null);
     } else {
         $(this).text('Đổi mật khẩu')
     }
 })
 // ---------------- ẩn thanh user nav khi kích thước màn hình nhỏ xuống  ----------------
-$(window).on('load', function() {
-    if ( window.innerWidth <= 768 ) {
+$(window).on('load', function () {
+    if (window.innerWidth <= 768) {
         $('.collapse').removeClass('show');
     }
-
-    $('body').on('click', '.btn-logout', function(e) {
-		localStorage.setItem('login-token', '');
-	})
-
-
-    $('#user-navigation-bar > li:first-child > a').addClass('active');
-    $('#user-navigation-bar > li > a').on('click', function(){
-        localStorage.setItem('user-tab-link', $(this).attr('href'));
-    })
 })
 
 // -------------- validate form --------------------
 $('#userForm').validate({
     rules: {
         'user-name': {
-            minlength:6,
+            minlength: 6,
             isValidUserName: true,
         },
         'full-name': {
             isValidName: true,
-        }, 
+        },
         'phone-number': {
-            minlength:10,
+            minlength: 10,
             isPhoneNumber: true,
         },
         'specific-address': {
@@ -67,7 +57,7 @@ $('#userForm').validate({
             minlength: 6,
             isPasswordSecure: true,
         },
-        'confirmed-password':{
+        'confirmed-password': {
             equalTo: '[name="new-password"]',
         }
     },
@@ -79,7 +69,7 @@ $('#userForm').validate({
         },
         'full-name': {
             required: 'Vui lòng nhập họ và tên',
-        }, 
+        },
         'phone-number': {
             required: 'Vui lòng nhập số điện thoại',
             minlength: 'Độ dài số điện thoại là 10'
@@ -88,13 +78,13 @@ $('#userForm').validate({
             required: 'Vui lòng nhập email',
             email: 'Định dạng email không hợp lệ'
         },
-        province:{
+        province: {
             required: 'Vui lòng chọn tỉnh/thành phố'
         },
-        district:{
+        district: {
             required: 'Vui lòng chọn quận/huyện'
         },
-        ward:{
+        ward: {
             required: 'Vui lòng chọn xã/phường'
         },
         'specific-address': {
@@ -108,13 +98,13 @@ $('#userForm').validate({
             minlength: 'Ít nhất 6 ký tự',
             isPasswordSecure: 'Chưa đủ độ an toàn (cần có cả chữ và số)',
         },
-        'confirmed-password':{
+        'confirmed-password': {
             required: 'Vui lòng xác nhận mật khẩu mới',
             equalTo: 'Mật khẩu xác nhận không khớp',
         }
     },
-    errorPlacement: function(errorElem, invalidElem){
-        if ( invalidElem.prop('tagName') == 'SELECT' ) {
+    errorPlacement: function (errorElem, invalidElem) {
+        if (invalidElem.prop('tagName') == 'SELECT') {
             errorElem.insertAfter(invalidElem.closest('.select-wrapper'));
         }
         else {
@@ -122,30 +112,37 @@ $('#userForm').validate({
         }
     }
 })
-jQuery.validator.addMethod( "isPasswordSecure", function(value) {
+jQuery.validator.addMethod("isPasswordSecure", function (value) {
     let regex = /^(?=.*[a-zA-Z])(?=.*[0-9])/g;
     return regex.test(value);
-}, 'Mật khẩu cần có ít nhất 1 chữ cái và 1 chữ số' )
+}, 'Mật khẩu cần có ít nhất 1 chữ cái và 1 chữ số')
 
-jQuery.validator.addMethod("isValidUserName", function(value){
+jQuery.validator.addMethod("isValidUserName", function (value) {
     const _1regex = /^[a-zA-Z]*$/g; // chỉ chứa chữ cái
     const _2regex = /^[a-zA-Z]+[a-zA-Z0-9]*$/g; // chỉ chứa chữ cái và số
 
     return _1regex.test(value) || _2regex.test(value);
 }, "Định dạng không hợp lệ")
 
-jQuery.validator.addMethod( "isPhoneNumber", function(value) {
+jQuery.validator.addMethod("isPhoneNumber", function (value) {
     const regex = /^0[\d]{9}$/gm;
-    return regex.test( value.trim() );
-}, 'Định dạng không hợp lệ' )
+    return regex.test(value.trim());
+}, 'Định dạng không hợp lệ')
 
-jQuery.validator.addMethod( "isValidName", function(value) {
+jQuery.validator.addMethod("isValidName", function (value) {
     const regex = /[^\p{L}\s]/gu;
-    return !regex.test( value.trim() );
-}, 'Định dạng không hợp lệ' )
+    return !regex.test(value.trim());
+}, 'Định dạng không hợp lệ')
 
-jQuery.validator.addMethod( "isValidAddressName", function(value) {
+jQuery.validator.addMethod("isValidAddressName", function (value) {
     const regex1 = /[^\p{L}\s\d,.-]/gu; // match string contains characters that are not unicode or number or letter or space,...
     const regex2 = /^[\d]+$/gm; // match string contains only number
-    return !regex1.test( value.trim() ) && !regex2.test( value.trim());
-}, 'Định dạng không hợp lệ' )
+    return !regex1.test(value.trim()) && !regex2.test(value.trim());
+}, 'Định dạng không hợp lệ')
+
+// Initialize Toast 
+const toastElem = document.querySelector('.toast');
+const toast = new bootstrap.Toast(toastElem, {
+    autohide: false,
+});
+toast.show();
--- user_order_info/js/main.js
+++ user_order_info/js/main.js
@@ -4,16 +4,4 @@ $(window).on('load', function () {
 		$('.collapse#user-navigation-bar').removeClass('show');
 	}
 
-	$('body').on('click', '.btn-logout', function (e) {
-		localStorage.setItem('login-token', '');
-	})
-
-	const tabLink = localStorage.getItem('user-tab-link');
-	if (tabLink == './user_order_info.html' || tabLink.includes('order')) {
-		$('#user-navigation-bar > li > a').filter('[href="./user_order_info.html"]').addClass('active');
-	}
-	$('#user-navigation-bar > li > a').on('click', function () {
-		localStorage.setItem('user-tab-link', $(this).attr('href'));
-	})
-
 })
